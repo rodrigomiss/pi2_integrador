b2844efd8e3cc7d71dea4c86cbe9e7504c280bcd
gustavohpk <ghpk88@gmail.com>
2014-05-11 02:31:40 -0300

    Iniciando layout do painel de admin

---
+++ admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<?php require_once "includes/head.php";?>	
+	<body>
+		<div class="container">	
+			<div class="row">
+
+				<div id="content" class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 ">
+					<h3>Painel do Administrador</h3>
+					<?php require_once "includes/admin/main-menu.php";?>
+				</div>
+			</div>
+		</div>
+
+	</body>
+
+	<?php require_once "includes/foot.php";?>
+
+</html>
