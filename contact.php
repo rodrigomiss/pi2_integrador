65455dd1095b6745de47407aadf47265ea50c6a9
gustavohpk <ghpk88@gmail.com>
2014-05-17 23:04:58 -0300

    Alterações na página de contato

--- contact.php
+++ contact.php
@@ -11,19 +11,19 @@
 				<div id="content" class="col-lg-12 col-xs-12 col-sm-12 col-md-12">
 					<h3>Contato</h3>
 					<div id="contact-form" class="row">
-						<div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-sm-offset-1">
+						<div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
 							<form>
 								<div class="input-group">
 									<span class="input-group-addon glyphicon glyphicon-user"></span>
-								  <input type="text" class="form-control" placeholder="Nome">
+								  <input type="text" class="form-control input-glyphicon" placeholder="Nome">
 								</div>
 								<div class="input-group">
 								  <span class="input-group-addon glyphicon glyphicon-envelope"></span>
-								  <input type="text" class="form-control" placeholder="E-mail">
+								  <input type="text" class="form-control input-glyphicon" placeholder="E-mail">
 								</div>
 								<div class="input-group">
 								  <span class="input-group-addon glyphicon glyphicon-asterisk"></span>
-								  <input type="textarea" class="form-control" placeholder="Mensagem">
+								  <input type="textarea" class="form-control input-glyphicon" placeholder="Mensagem">
 								</div>
 								<button type="submit" class="btn btn-default btn-lg">Enviar</button>
 							</form>
